9a867ad3ab3505628b03449dfc80a2cfadb687e0
Leon <Leon@margale.de>
2022-12-19 23:48:31 +0100

    Initial Commit

---
+++ WebserverC/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  WebserverC
+//
+//  Created by Leon Margale on 19.12.22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
